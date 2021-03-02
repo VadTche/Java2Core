78cf8ec9b4e7a3fe455a43d3da3d61cfc8943269
VadTche <pismonos@gmail.com>
2021-03-02 18:48:50 +0300

    HomeWork8

---
+++ src/main/java/Lesson8/DatabaseRepository.java
@@ -0,0 +1,13 @@
+package Lesson8;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+// Репозиторий для работы
+public interface DatabaseRepository {
+
+    boolean saveWeatherData(WeatherData weatherData) throws SQLException;
+    List<WeatherData> getAllSavedData() throws IOException;
+}
+
